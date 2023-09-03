ab7dbd59fbeadbc64e9025633ab882c14350f5fc
shivam01anand <42206227+shivam01anand@users.noreply.github.com>
2023-09-04 00:09:00 +0530

    ga

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,8 +1,9 @@
 export const metadata = {
-  title: 'Home - Simple',
+  title: 'Easy Cover Letter',
   description: 'Page description',
 }
 
+import Head from 'next/head';
 import Hero from '@/components/hero'
 import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
@@ -12,6 +13,19 @@ import Newsletter from '@/components/newsletter'
 export default function Home() {
   return (
     <>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-4N4Y02Y9NL"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-4N4Y02Y9NL');
+              `
+            }}
+          />
+        </Head>
       <Hero />
       {/* <Features /> */}
       {/* <FeaturesBlocks /> */}
